0f762a4d8d67d7cc3919f683c0af34e2e8396e75
benzoic09 <104141553+benzoic09@users.noreply.github.com>
2024-08-09 17:05:15 +0300

    css linkage

--- Portfolio/myapp/views.py
+++ Portfolio/myapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index (request):
     return render(request, 'index.html')
+
+def apple(request):
+    return render(request, 'apple2024.html')
